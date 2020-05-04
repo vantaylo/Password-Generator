ffbdfc1377c46c726235b83fdb9c5524e3f8df50
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-05-03 23:24:42 -0500

    document object variables

--- passwords.js
+++ passwords.js
@@ -1,9 +1,30 @@
 //variables
 var letters = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
-var sym = ''"~`!@#$%^&*()_+=[]\{}|;':,./<>?"'
+var sym = "!@#$%^&*()_+=[]{}|;':./,<>?";
 
-//document object.
+//document object variables
+var charLength = (document.getElementById(charLength).onclick = function () {
+  code;
+});
+var numSwitch = (document.getElementById(num).onclick = function () {
+  code;
+});
+var capitalSwitch = (document.getElementById(capital).onclick = function () {
+  code;
+});
+var lowerSwitch = (document.getElementById(lower).onclick = function () {
+  code;
+});
+var symSwitch = (document.getElementById(sym).onclick = function () {
+  code;
+});
+var generateBtn = (document.getElementById(generate).onclick = function () {
+  code;
+});
+var display = (document.getElementById(display).onclick = function () {
+  code;
+});
 
 //submit function
 
