7dba5d9aef3d9163f6d1b522e00378eff38da6ad
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-05-07 00:17:09 -0500

    using if statements, if checkbox is true

--- passwords.js
+++ passwords.js
@@ -1,7 +1,9 @@
 //variables
-var letters = "abcdefghijklmnopqrstuvwxyz";
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var sym = "!@#$%^&*()_+=[]{}|;':./,<>?";
+var newPassword = [];
 
 //capture slider value
 var passSliderLength = document.getElementById("length");
@@ -18,4 +20,30 @@ generateBtn.addEventListener("mouseup", function () {
   var passCaps = document.getElementById("capitalSwitch").checked;
   var passLower = document.getElementById("lowerSwitch").checked;
   var passSym = document.getElementById("symSwitch").checked;
+
+  // using if statements, if checkbox is true,
+  // add that string to passwordPossiblities
+  var passwordPossibilities = "";
+
+  if (passNum === true) {
+    passwordPossibilities += numbers;
+  }
+  if (passCaps === true) {
+    passwordPossibilities += upperCaseLetters;
+  }
+  if (passLower === true) {
+    passwordPossibilities += lowerCaseLetters;
+  }
+  if (passSym === true) {
+    passwordPossibilities += sym;
+  }
+
+  var result = "";
+
+  // After you generate all the possiblities
+  // loop "passLength" times and pick and random
+  // index between zero and passwordPossibilities.length
+  // from password possibilies and add it to result
+
+  // return result
 });
